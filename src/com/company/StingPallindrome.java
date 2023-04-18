51ca983ff9248b3a668196fa4f857ae4944b11e1
Suraj Kumar <surajsrivastava021194@gmail.com>
2023-04-18 10:13:43 +0530

    Third Commit

--- src/com/company/StingPallindrome.java
+++ src/com/company/StingPallindrome.java
@@ -4,10 +4,19 @@ public class StingPallindrome {
     public static void main(String[] args) {
         String text = "Suraj Is my name";
         String rev ="";
+        String text1 = text;
         for(int i=text.length(); i>0; i--){
             rev = rev + text.charAt(i-1);
         }
         System.out.println(rev);
         System.out.println(text);
+
+        if(rev==text1){
+            System.out.println("The string is pallindrome");
+        }
+        else{
+            System.out.println("The string is not pallindrome");
+
+        }
     }
 }
